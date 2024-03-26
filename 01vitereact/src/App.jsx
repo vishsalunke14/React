d794c90bdf73f61070c37c5e20e3e607b6e5b35c
vishsalunke14 <vishsalunke14@gmail.com>
2024-03-26 18:07:26 +0530

    using vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+function App() {
+  const username = "chai aur code"
+  return (
+    <>
+
+      <Chai />
+      <h1>chai aur react {username}</h1>
+      <p>greag</p>
+      
+    </>
+  )
+}
+
+export default App
