aee7034b596704e161a619673fde8109eb11184a
vishsalunke14 <vishsalunke14@gmail.com>
2024-04-22 19:05:49 +0530

    changes in card

--- 09themeSwitcher/src/components/Card.jsx
+++ 09themeSwitcher/src/components/Card.jsx
@@ -3,7 +3,7 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/20780453/pexels-photo-20780453/free-photo-of-a-close-up-of-a-pink-cherry-blossom-tree.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                <img className="p-8 rounded-t-lg" src="https://m.media-amazon.com/images/I/71-i7pbrhnL._SL1500_.jpg" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
